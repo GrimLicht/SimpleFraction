5ffa93779d7e78626ff4e604879190e0181f3d38
Utilisateur <user@debian.arcahe.ovh>
2020-09-25 10:51:28 +0200

    essai n°3

---
+++ Main2.java
@@ -0,0 +1,15 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello World!\n");
+
+        Fraction fraction = new Fraction(1,2);
+
+		//System.out.println("Oh Romeo my Romeo!\n");
+		
+        fraction.toString();
+	}
+
+}
